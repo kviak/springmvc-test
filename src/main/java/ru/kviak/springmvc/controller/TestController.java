7873540fcf2e8ec17e8fd4906966254d8437ebe6
Denis Lapin <kviakit@Gmail.com>
2023-08-03 22:59:27 +0300

    without xml

--- src/main/java/ru/kviak/springmvc/controller/TestController.java
+++ src/main/java/ru/kviak/springmvc/controller/TestController.java
@@ -1,4 +1,4 @@
-package ru.kviak.springmvc.Controller;
+package ru.kviak.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
