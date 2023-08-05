75d45ad86efcd1eaf37049fb533981ffd05d7e5d
Denis Lapin <kviakit@Gmail.com>
2023-08-05 22:17:49 +0300

    CRUD, REST, DAO (part 3). Update, Delete.

--- src/main/java/ru/kviak/springmvc/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/ru/kviak/springmvc/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,5 +1,8 @@
 package ru.kviak.springmvc.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -15,5 +18,15 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
