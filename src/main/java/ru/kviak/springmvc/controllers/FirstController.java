49665aaccf388861526fd53a1f781273219fc859
Denis Lapin <kviakit@Gmail.com>
2023-08-05 11:22:54 +0300

    request params

--- src/main/java/ru/kviak/springmvc/controllers/FirstController.java
+++ src/main/java/ru/kviak/springmvc/controllers/FirstController.java
@@ -1,13 +1,18 @@
 package ru.kviak.springmvc.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false)  String name){
+        System.out.println(name);
         return "first/hello";
     }
     @GetMapping("/goodbye")
--- src/main/java/ru/kviak/springmvc/controllers/SecondController.java
+++ src/main/java/ru/kviak/springmvc/controllers/SecondController.java
@@ -2,9 +2,11 @@ package ru.kviak.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/second")
 public class SecondController {
     @GetMapping("/exit")
     public String exit(){
