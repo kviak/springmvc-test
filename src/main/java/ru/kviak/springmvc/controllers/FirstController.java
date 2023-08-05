aad8ce66acecc7e4f843286b7fd46733b10d62b1
Denis Lapin <kviakit@Gmail.com>
2023-08-05 13:42:33 +0300

    Transfer model from controller, to view.

--- src/main/java/ru/kviak/springmvc/controllers/FirstController.java
+++ src/main/java/ru/kviak/springmvc/controllers/FirstController.java
@@ -1,7 +1,7 @@
 package ru.kviak.springmvc.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,12 +11,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false)  String name){
-        System.out.println(name);
+    public String helloPage(@RequestParam(value = "name", required = false)  String name, Model model){
+        model.addAttribute("message", name);
         return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calc(@RequestParam("a") int a, @RequestParam("b") int b,
+                       @RequestParam("action") String action, Model model){
+
+        int answer;
+
+        switch (action) {
+            case "plus" -> answer = a + b;
+            case "minus" -> answer = a - b;
+            case "multip" -> answer = a * b;
+            case "divis" -> answer = a / b;
+            default -> answer=0;
+        }
+        model.addAttribute("answer", answer);
+        return "first/calculator";
     }
 }
