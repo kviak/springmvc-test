1ed2f5b22ed28023af0bf791a1d1ec354886daa3
Denis Lapin <kviakit@Gmail.com>
2023-08-05 21:28:44 +0300

    CRUD, REST, DAO (part 2). ModelAttribute.

--- src/main/java/ru/kviak/springmvc/controllers/PeopleController.java
+++ src/main/java/ru/kviak/springmvc/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.kviak.springmvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.kviak.springmvc.dao.PersonDAO;
+import ru.kviak.springmvc.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -18,7 +17,6 @@ public class PeopleController {
         this.personDAO = personDAO;
     }
 
-
     @GetMapping()
     public String index(Model model){
         model.addAttribute("people", personDAO.index());
@@ -29,4 +27,14 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
 }
--- src/main/java/ru/kviak/springmvc/dao/PersonDAO.java
+++ src/main/java/ru/kviak/springmvc/dao/PersonDAO.java
@@ -27,4 +27,9 @@ public class PersonDAO {
                 .findAny()
                 .orElse(null);
     }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
 }
