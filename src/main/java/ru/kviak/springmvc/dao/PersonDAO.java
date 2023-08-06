dbac4c73d843906caf0b284dbbf52b7e9505b486
Denis Lapin <kviakit@Gmail.com>
2023-08-06 22:20:42 +0300

    Test BatchUpdate.

--- src/main/java/ru/kviak/springmvc/dao/PersonDAO.java
+++ src/main/java/ru/kviak/springmvc/dao/PersonDAO.java
@@ -1,10 +1,15 @@
 package ru.kviak.springmvc.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.kviak.springmvc.models.Person;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -15,8 +20,8 @@ public class PersonDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-
     public List<Person> index(){
+
         return jdbcTemplate.query("SELECT * FROM Person",
                         new BeanPropertyRowMapper<>(Person.class));
     }
@@ -35,10 +40,60 @@ public class PersonDAO {
     }
     public void update(int id, Person updatePerson){
         jdbcTemplate.update("UPDATE Person SET name=?, email=?, age=? WHERE  id=?",
-                updatePerson.getName(), updatePerson.getEmail(), updatePerson.getAge(), updatePerson.getId());
+                updatePerson.getName(), updatePerson.getEmail(), updatePerson.getAge(), id);
     }
 
     public void delete(int id){
         jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
+
+
+    public void testMultipleUpdate(){
+        List<Person> people = create1000people();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person: people){
+            jdbcTemplate.update("INSERT INTO Person VALUES (?,?,?,?)", person.getId(),
+                    person.getName(), person.getEmail(), person.getAge());
+        }
+
+        long after = System.currentTimeMillis();
+
+        System.out.println(after-before);
+    }
+
+    public void testBatchUpdate(){
+        List<Person> people = create1000people();
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES(?,?,?,?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, people.get(i).getId());
+                        ps.setString(2, people.get(i).getName());
+                        ps.setString(3, people.get(i).getEmail());
+                        ps.setInt(4, people.get(i).getAge());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+        System.out.println(after-before);
+    }
+
+    private List<Person> create1000people() {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, i+"a", i+10, i+"asd"+"@.mail.ru"));
+        }
+        return people;
+    }
+
 }
