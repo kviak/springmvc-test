3b451d041b23d8f5e5bbd3eb8793bce0ba3cebc1
Denis Lapin <kviakit@Gmail.com>
2023-08-15 12:15:32 +0300

    Validator microfix.

--- src/main/java/ru/kviak/springmvc/dao/PersonDAO.java
+++ src/main/java/ru/kviak/springmvc/dao/PersonDAO.java
@@ -31,8 +31,12 @@ public class PersonDAO {
         Session session = sessionFactory.getCurrentSession();
         return session.get(Person.class, id);
     }
+    @Transactional
     public Optional<Person> show(String email){
-        return Optional.empty();
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("select p from Person p where p.email ="+"'"+email + "'", Person.class)
+                .getResultList()
+                .stream().findAny();
     }
     @Transactional
     public void save(Person person){
@@ -52,52 +56,4 @@ public class PersonDAO {
         Session session = sessionFactory.getCurrentSession();
         session.remove(session.get(Person.class, id));
     }
-
-
-//    public void testMultipleUpdate(){
-//        List<Person> people = create1000people();
-//
-//        long before = System.currentTimeMillis();
-//
-//        for (Person person: people){
-//            jdbcTemplate.update("INSERT INTO Person(name, email, age) VALUES (?,?,?)",
-//                    person.getName(), person.getEmail(), person.getAge());
-//        }
-//
-//        long after = System.currentTimeMillis();
-//
-//        System.out.println(after-before);
-//    }
-//
-//    public void testBatchUpdate(){
-//        List<Person> people = create1000people();
-//        long before = System.currentTimeMillis();
-//
-//        jdbcTemplate.batchUpdate("INSERT INTO Person(name, email, age) VALUES (?,?,?)",
-//                new BatchPreparedStatementSetter() {
-//                    @Override
-//                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-//                        ps.setString(1, people.get(i).getName());
-//                        ps.setString(2, people.get(i).getEmail());
-//                        ps.setInt(3, people.get(i).getAge());
-//                    }
-//
-//                    @Override
-//                    public int getBatchSize() {
-//                        return people.size();
-//                    }
-//                });
-//
-//        long after = System.currentTimeMillis();
-//        System.out.println(after-before);
-//    }
-//
-//    private List<Person> create1000people() {
-//        List<Person> people = new ArrayList<>();
-//
-//        for (int i = 0; i < 1000; i++) {
-//            people.add(new Person(i, i+"a", i+10, i+"asd"+"@.mail.ru"));
-//        }
-//        return people;
-//    }
 }
